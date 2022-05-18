7c558ed03238d479d36d47bdf97e93ee8c76e9a6
BButner <butner.beau@gmail.com>
2022-05-18 11:05:31 -0500

    Verify top-level key duplicates in the config

--- src/lib/config/mod.rs
+++ src/lib/config/mod.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use core::panic;
 use std::fs;
 pub mod command_types;
 
@@ -27,7 +28,24 @@ impl MacroType {
 pub fn load_config() -> TaranConfig {
     let data = fs::read_to_string("./config.json").expect("Unable to read config...");
 
-    serde_json::from_str(&data).unwrap()
+    let config = serde_json::from_str(&data).unwrap();
+
+    verify_config(&config);
+
+    return config;
+}
+
+fn verify_config(config: &TaranConfig) {
+    let macros = &config.macros.clone();
+    let mut visited: Vec<String> = Vec::new();
+
+    for m in macros.iter() {
+        if visited.contains(m.key()) {
+            panic!("Found dupilicate top-level macro key: {}", m.key());
+        }
+
+        visited.push(m.key().to_string());
+    }
 }
 
 #[derive(Clone, Deserialize, Serialize, Debug)]
