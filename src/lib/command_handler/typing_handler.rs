999453876f129296b8e766eec9082e2a5c125654
BButner <butner.beau@gmail.com>
2022-06-14 13:18:56 -0500

    Trying out simulate

--- src/lib/command_handler/typing_handler.rs
+++ src/lib/command_handler/typing_handler.rs
@@ -13,26 +13,27 @@ pub fn handle_typing_macro(cmd: &Typing) {
 }
 
 pub fn type_macro(text: &String) {
-    let mode: TypingMode = if cfg!(target_os = "windows") {
-        TypingMode::KeyDownUp
-    } else {
-        TypingMode::KeySequence
-    };
-
-    let mut enigo = Enigo::new();
-
-    match mode {
-        TypingMode::KeySequence => enigo.key_sequence(text),
-        TypingMode::KeyDownUp => {
-            for c in text.chars() {
-                if c.is_uppercase() { enigo.key_down(Key::Shift) }
-
-                let lowercase = c.to_lowercase().nth(0).unwrap();
-
-                enigo.key_down(Key::Layout(lowercase));
-                enigo.key_up(Key::Layout(lowercase));
-                if c.is_uppercase() { enigo.key_up(Key::Shift) }
-            }
-        }
-    }
+    simulate::type_str(&text);
+    // let mode: TypingMode = if cfg!(target_os = "windows") {
+    //     TypingMode::KeyDownUp
+    // } else {
+    //     TypingMode::KeySequence
+    // };
+    //
+    // let mut enigo = Enigo::new();
+    //
+    // match mode {
+    //     TypingMode::KeySequence => enigo.key_sequence(text),
+    //     TypingMode::KeyDownUp => {
+    //         for c in text.chars() {
+    //             if c.is_uppercase() { enigo.key_down(Key::Shift) }
+    //
+    //             let lowercase = c.to_lowercase().nth(0).unwrap();
+    //
+    //             enigo.key_down(Key::Layout(lowercase));
+    //             enigo.key_up(Key::Layout(lowercase));
+    //             if c.is_uppercase() { enigo.key_up(Key::Shift) }
+    //         }
+    //     }
+    // }
 }
